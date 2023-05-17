0d4499bbdc94f120b412358d03ed793c52080d5a
IvanMiyasoied <121316974+IvanMiyasoied@users.noreply.github.com>
2023-05-17 23:47:28 +0300

    first

---
+++ app.js
@@ -0,0 +1,61 @@
+// function factorial(n) {
+//     if (n === 0 || n === 1) 
+//         return 1;
+    
+//     for (i = n-1; i >= 1; i--) {
+
+//         n*=i;
+//     }
+    
+//     return n;
+// }
+
+// factorial(5);
+// console.log(factorial(5));
+
+
+
+// function factorial(n) {
+//     if ( n < 0)
+//       return -1; 
+//       else if ( n == 0 )
+//       return 1;
+//       else {
+//         return (n * factorial(n-1));
+//       } 
+// }
+
+// factorial(5);
+// console.log(factorial(5));
+
+
+
+// arr = [1,2,3,4,5]
+
+// function getSum(arr) {
+//     let sum = arr.shift();
+
+//     if (arr.length !==0) {
+//         sum += getSum(arr);
+//     }
+//     return sum
+// }
+
+// getSum(arr);
+// console.log(getSum([1,2,3,4,5]));
+
+
+
+
+// let getGlob = () => {
+//     console.log(window);
+// };
+
+// if ( !window.Promise) {
+//     window.Promise = ...
+// }
+
+// getGlob();
+
+
+
